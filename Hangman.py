b113dab08fed6e152e02916e6cfdf86ddff28dc9
IbrahimAlrayes <96233417+IbrahimAlrayes@users.noreply.github.com>
2022-01-16 00:08:48 +0300

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,99 @@
+import random
+
+def hangman():
+    word = random.choice(["yasser", "fisal","mohammed","abdullah","ibrahim","faris","fahad"])
+    valid = "asdfghjklzxcvbnmqwertyuiop"
+    turns = 10
+    guessmade = ''
+    while len(word)>0:
+        main = ""
+        guess = ""
+        for letter in word:
+            if letter in guessmade:
+                main += letter 
+            else:
+                main = main + "_" + " "
+
+        print(main)
+
+        if main == word:
+            print(main)
+            print("You won")
+            break
+        
+        print("Guess the word:" , main)
+        guess = input()
+
+        if guess in valid:
+            guessmade+= guess
+        else:
+            print("enter valid input:")
+            guess = input()
+        
+        
+        if guess not in word:
+            turns = turns - 1
+            if turns == 9:
+                print("9 turns left")
+                print("  --------  ")
+            if turns == 8:
+                print("8 turns left")
+                print("  --------  ")
+                print("     O      ")
+            if turns == 7:
+                print("7 turns left")
+                print("  --------  ")
+                print("     O      ")
+                print("     |      ")
+            if turns == 6:
+                print("6 turns left")
+                print("  --------  ")
+                print("     O      ")
+                print("     |      ")
+                print("    /       ")
+            if turns == 5:
+                print("5 turns left")
+                print("  --------  ")
+                print("     O      ")
+                print("     |      ")
+                print("    / \     ")
+            if turns == 4:
+                print("4 turns left")
+                print("  --------  ")
+                print("   \ O      ")
+                print("     |      ")
+                print("    / \     ")
+            if turns == 3:
+                print("3 turns left")
+                print("  --------  ")
+                print("   \ O /    ")
+                print("     |      ")
+                print("    / \     ")
+            if turns == 2:
+                print("2 turns left")
+                print("  --------  ")
+                print("   \ O /|   ")
+                print("     |      ")
+                print("    / \     ")
+            if turns == 1:
+                print("1 turns left")
+                print("Last breaths counting, Take care!")
+                print("  --------  ")
+                print("   \ O_|/   ")
+                print("     |      ")
+                print("    / \     ")
+            if turns == 0:
+                print("You loose")
+                print("You let a kind man die")
+                print("  --------  ")
+                print("     O_|    ")
+                print("    /|\      ")
+                print("    / \     ")
+                break
+        
+
+
+
+hangman()
+print()
+    
